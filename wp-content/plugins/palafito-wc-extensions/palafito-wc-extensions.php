c1c2c73ba463f9bc03fa3e81ceb46bfe99e82504
Arturo Ramirez <arturo6ramirez@gmail.com>
2025-06-30 00:33:11 +0200

    primer hook test

--- wp-content/plugins/palafito-wc-extensions/palafito-wc-extensions.php
+++ wp-content/plugins/palafito-wc-extensions/palafito-wc-extensions.php
@@ -14,3 +14,14 @@ add_action('init', function () {
     // Puedes dejar este mensaje para saber que el plugin carga bien
     error_log('[Palafito WC Extensions] Plugin cargado correctamente');
 });
+
+/**
+ * Añadir un aviso personalizado en el checkout de WooCommerce.
+ */
+add_action('woocommerce_before_checkout_form', 'palafito_custom_checkout_notice');
+
+function palafito_custom_checkout_notice() {
+    if (is_checkout()) {
+        echo '<div class="woocommerce-info">Gracias por confiar en Palafito. Recuerda que todos los pedidos se despachan en 24-48h 🚀</div>';
+    }
+}
