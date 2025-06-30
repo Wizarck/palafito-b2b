81d683367df24ddd29e36f315dd84b52d7993fad
Arturo Ramirez <arturo6ramirez@gmail.com>
2025-07-01 00:07:52 +0200

    fix(checkout): hide shipping form visually, make all fields optional (ascii only)

--- wp-content/themes/palafito-child/woocommerce/checkout/form-shipping.php
+++ wp-content/themes/palafito-child/woocommerce/checkout/form-shipping.php
@@ -3,22 +3,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 ?>
-<div class="woocommerce-shipping-fields">
+<div class="woocommerce-shipping-fields" style="display: none;">
 	<?php do_action( 'woocommerce_before_checkout_shipping_form', $checkout ); ?>
 	<div class="woocommerce-shipping-fields__field-wrapper">
 		<?php
 		$fields = $checkout->get_checkout_fields( 'shipping' );
 		foreach ( $fields as $key => &$field ) {
+			$field['required'] = false;
 			if ( $key === 'shipping_phone' ) {
-				$field['required'] = true;
 				$field['label'] = __( 'Teléfono', 'woocommerce' );
 			} elseif ( $key === 'shipping_country' ) {
-				$field['required'] = true;
 				$field['label'] = __( 'País', 'woocommerce' );
-			} elseif ( in_array( $key, array( 'shipping_company', 'shipping_last_name' ), true ) ) {
-				$field['required'] = false;
-			} else {
-				$field['required'] = true;
 			}
 			if (empty($field['class'])) {
 				$field['class'] = array('form-row-wide');
