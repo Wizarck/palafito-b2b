e5eb6d835e0d66c37bc9ad9eb32a4814add7164a
Arturo Ramirez <arturo6ramirez@gmail.com>
2025-06-30 18:30:45 +0200

    fix: usar plugin_basename(__FILE__) en declaraci��n HPOS para compatibilidad WooCommerce

--- wp-content/plugins/palafito-wc-extensions/palafito-wc-extensions.php
+++ wp-content/plugins/palafito-wc-extensions/palafito-wc-extensions.php
@@ -52,7 +52,11 @@ add_action(
 	'before_woocommerce_init',
 	function () {
 		if ( class_exists( '\\Automattic\\WooCommerce\\Utilities\\FeaturesUtil' ) ) {
-			\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+			\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility(
+				'custom_order_tables',
+				plugin_basename( __FILE__ ),
+				true
+			);
 		}
 	}
 );
