72c0c509fc2f15ee862d5359c67d9d93ea5281d5
Arturo Ramirez <arturo6ramirez@gmail.com>
2025-07-02 03:10:04 +0200

    feat: remove shipping address display from cart shipping row

--- wp-content/themes/palafito-child/woocommerce/cart/cart.php
+++ wp-content/themes/palafito-child/woocommerce/cart/cart.php
@@ -198,6 +198,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 		/**
 		 * Cart collaterals hook.
 		 *
+		 * @hooked woocommerce_cross_sell_display
 		 * @hooked woocommerce_cart_totals - 10
 		 */
 		do_action( 'woocommerce_cart_collaterals' );
