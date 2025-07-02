e604467a65ab29751d3904fa36f229a2e73eb7ac
Arturo Ramirez <arturo6ramirez@gmail.com>
2025-07-02 03:05:10 +0200

    feat: add customer order notes field to checkout page

--- wp-content/themes/palafito-child/woocommerce/checkout/form-checkout.php
+++ wp-content/themes/palafito-child/woocommerce/checkout/form-checkout.php
@@ -37,6 +37,25 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 		<div class="palafito-checkout-left" style="flex: 1 1 350px; min-width: 320px;">
 			<h3><?php esc_html_e( 'Dirección de envío', 'woocommerce' ); ?></h3>
 			<?php do_action( 'woocommerce_checkout_shipping' ); ?>
+			
+			<!-- Sección de notas de cliente -->
+			<div class="woocommerce-additional-fields">
+				<?php do_action( 'woocommerce_before_order_notes', $checkout ); ?>
+
+				<?php if ( apply_filters( 'woocommerce_enable_order_notes_field', 'yes' === get_option( 'woocommerce_enable_order_comments', 'yes' ) ) ) : ?>
+
+					<h3><?php esc_html_e( 'Información adicional', 'woocommerce' ); ?></h3>
+
+					<div class="woocommerce-additional-fields__field-wrapper">
+						<?php foreach ( $checkout->get_checkout_fields( 'order' ) as $key => $field ) : ?>
+							<?php woocommerce_form_field( $key, $field, $checkout->get_value( $key ) ); ?>
+						<?php endforeach; ?>
+					</div>
+
+				<?php endif; ?>
+
+				<?php do_action( 'woocommerce_after_order_notes', $checkout ); ?>
+			</div>
 		</div>
 		<div class="palafito-checkout-right" style="flex: 1 1 350px; min-width: 320px; max-width: 500px;">
 			<h3><?php esc_html_e( 'Pedido', 'woocommerce' ); ?></h3>
